0083d75750acc89d1784c8923aa1013117488ad4
Artem Tkachenko <tkachenko@agent.ru>
2014-05-29 22:58:03 +0400

    helloworlt

---
+++ tasks/teamcityDeploy.js
@@ -0,0 +1,16 @@
+/*
+ * grunt-teamcityDeploy
+ * https://github.com/tomsoir/teamcityDeploy
+ *
+ * Copyright (c) 2014 Artem Tkachenko
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+    grunt.registerMultiTask('teamcityDeploy', 'Custom grunt plugin for teamCity and autodeploy source', function(bashParamEnvPath) {
+        console.log("HELLOWORLD");
+    });
+};
